228fa189a4605072c8f2d6e7c922a232ae1d5bbb
shacojx <shacojx001@gmail.com>
2019-11-09 23:59:28 +0700

    fix bug

--- src/main/java/function/CheckSiteAdmin.java
+++ src/main/java/function/CheckSiteAdmin.java
@@ -51,9 +51,4 @@ public class CheckSiteAdmin {
             }
         }
  
-        public static void main(String[] args) {
-            CheckSiteAdmin checkSite = new CheckSiteAdmin();
-            checkSite.checkSiteAdmin();
- 
-        }
     }
